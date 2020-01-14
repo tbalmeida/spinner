257367045e69b35c63381e9b5bce181060a25f62
Thiago Almeida <tbalmeida@gmail.com>
2020-01-14 21:01:13 +0000

    Pair coding

--- spinner2.js
+++ spinner2.js
@@ -2,15 +2,15 @@ process.stdout.write('hello from spinner1.js... \rheyyy\n');
 
 const vTxt = "|/-\|/-\\|"
 
-let delay = 50;
+let delay = 200;
 
 for (const char of vTxt) {
   setTimeout(() => {
     process.stdout.write("\r" + char);
   }, delay);
-  delay += 200;
+  delay += 150;
 }
 
 setTimeout (() =>{
   process.stdout.write('\n')
-}, delay += 200);
+}, delay += 150);
